9de1c8c8c427c3052ed39935336889eb7e139281
dilasals611-png <dilasals611@gmail.com>
2026-09-01 15:04:51 +0700

    Add HTML structure with internal JavaScript example

---
+++ script.js
@@ -0,0 +1,28 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Internal JavaScript</title>
+</head>
+<body>
+    <h1 style="text-align;">ini adalah halaman latihan internal</h1>
+    
+    <h3>Berikut ini adalah contoh penggunaan eksternal JS</h3>
+    <button onclick="tampilkanPesan()">Klik saya!</button>
+    <hr>
+
+    <button onclick="alert('ini tombol kedua menggunakan inline JS');">
+        tombol kedua
+    </button>
+ 
+    <!--contoh internal JavaScript-->
+    <script>
+        alert("selamat datang di pembelajaran JavaScript!");
+    </script>
+
+    <!--contoh pemanggilan javascript-->
+    <script src="script.js"></script>
+
+</body>
+</html>
